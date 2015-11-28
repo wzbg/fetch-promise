c82f19ef3f553081026c6e517048164bc29e79bc
zyc <z35305515@gmail.com>
2015-11-29 03:40:30 +0800

    fetchUrl again by encodeURI

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const fetchUrl = require('fetch').fetchUrl;
 module.exports = function (url, options) {
   return new Promise((resolve, reject) => {
     fetchUrl(url, options, (err, res, buf) => {
-      if (err) reject(err);
+      if (err || res.status != 200 || !buf) {
+        fetchUrl(encodeURI(url), options, (err, res, buf) => {
+          if (err) reject(err);
+          else resolve({ res, buf });
+        });
+      }
       else resolve({ res, buf });
     });
   });
